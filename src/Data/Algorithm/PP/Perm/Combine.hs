f5ca8b46f0a9db3ae1860fc07d5e9ef8bb52109e
vialette <vialette@gmail.com>
2018-11-18 21:51:22 +0100

    permutation simple and composition

--- src/Data/Algorithm/PP/Perm/Combine.hs
+++ src/Data/Algorithm/PP/Perm/Combine.hs
@@ -15,6 +15,7 @@ where
 
   import qualified Data.Algorithm.PP.Perm         as PP.Perm
 
+  -- |
   (/+/) :: PP.Perm.Perm -> PP.Perm.Perm -> PP.Perm.Perm
   p /+/ q = PP.Perm.mk (xs ++ ys)
     where
@@ -22,9 +23,11 @@ where
       np  = L.length xs
       ys = L.map (+np) $ PP.Perm.toList q
 
+  -- |
   directSum :: PP.Perm.Perm -> PP.Perm.Perm -> PP.Perm.Perm
   directSum = (/+/)
 
+  -- |
   (/-/) :: PP.Perm.Perm -> PP.Perm.Perm -> PP.Perm.Perm
   p /-/ q = PP.Perm.mk (xs ++ ys)
     where
@@ -32,9 +35,11 @@ where
       xs = L.map (+nq) $ PP.Perm.toList p
       ys = PP.Perm.toList q
 
+  -- |
   skewSum :: PP.Perm.Perm -> PP.Perm.Perm -> PP.Perm.Perm
   skewSum = (/+/)
 
+  -- |
   (/./) :: PP.Perm.Perm -> PP.Perm.Perm -> PP.Perm.Perm
   p /./ q = L.map T.snd . L.sortBy cmpFst) . map proj . L.zip ixs . L.sortBy (cmpSnd) $ iys
     where
@@ -44,5 +49,6 @@ where
       ixs        = L.zip [1..] $ PP.Perm.toList p
       iys        = L.zip [1..] $ PP.Perm.toList q
 
+  -- |
   dot :: PP.Perm.Perm -> PP.Perm.Perm -> PP.Perm.Perm
   dot = (/./)
