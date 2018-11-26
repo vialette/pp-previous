38a29b67809c2b1e709f63b92cb86dbbfe497655
vialette <vialette@gmail.com>
2018-11-26 10:25:13 +0100

    Add Geometry.Point.hs

--- src/Data/Algorithm/PP/Perm/Run.hs
+++ src/Data/Algorithm/PP/Perm/Run.hs
@@ -7,11 +7,17 @@ where
   import qualified Control.Arrow as A
   import qualified Data.Foldable as F
   import qualified Data.List     as L
+  import qualified Data.List.Split as L.Split
   import qualified Data.Tuple    as T
 
   import qualified Data.Algorithm.PP.Combi      as PP.Combi
   import qualified Data.Algorithm.PP.Perm       as PP.Perm
   import qualified Data.Algorithm.PP.Utils.List as PP.Utils.List
 
-  ascendingRuns :: PP.Perm.Perm -> [PP.Perm.Pattern]
-  ascendingRuns = PP.Perm.toList
+  --ascendingRuns :: PP.Perm.Perm -> [PP.Perm.Pattern]
+  ascendingRuns = aux [] . PP.Utils.List.chunk2 . PP.Perm.toList
+    where
+      aux acc [] = L.reverse
+      aux [] [(y, y')] = aux [y, y']
+      aux [] ((y, _) : ys) = aux [y] ys
+      aux
