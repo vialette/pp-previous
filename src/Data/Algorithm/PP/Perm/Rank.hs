c5888f2e354d07396a504850a4fbf833df868f5d
vialette <vialette@gmail.com>
2018-11-27 22:33:58 +0100

    PP.Perm.Rank.hs

--- src/Data/Algorithm/PP/Perm/Rank.hs
+++ src/Data/Algorithm/PP/Perm/Rank.hs
@@ -8,17 +8,20 @@ module Data.Algorithm.PP.Perm.Rank
 )
 where
 
-  import qualified Data.Algorithm.PP.Perm       as PP.Perm
+  import qualified Data.Algorithm.PP.Perm as PP.Perm
 
+  -- |'rank1' 'p'
   rank1 :: PP.Perm.Perm -> Int
   rank1 p = 0
 
+  -- |'unrank1' 'k' 'n'
   unrank1 :: Int -> Int -> PP.Perm.Perm
   unrank1 _ _ = PP.Perm.identity 5
 
-
+  -- |'rank2' 'p'
   rank2 :: PP.Perm.Perm -> Int
   rank2 p = 0
 
+  -- |'unrank2' 'k' 'n'
   unrank2 :: Int -> Int -> PP.Perm.Perm
   unrank2 _ _ = PP.Perm.identity 5
