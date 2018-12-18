f83adfbe0e621d3759c0aa766946a7a270b27a30
vialette <vialette@gmail.com>
2018-12-18 14:26:42 +0100

    random rank/unrank

--- src/Data/Algorithm/PP/Perm/Rank.hs
+++ src/Data/Algorithm/PP/Perm/Rank.hs
@@ -1,11 +1,14 @@
 module Data.Algorithm.PP.Perm.Rank
 (
+  -- * Algorithm 1 from Myrvold, and Ruskey, IPL, 2001
   rank1
 , unrank1
 
+  -- * Algorithm 2 from Myrvold, and Ruskey, IPL, 2001
 , rank2
 , unrank2
 
+  -- * Random rank/unrank
 , rand
 )
 where
@@ -18,17 +21,22 @@ where
   import qualified Data.Algorithm.PP.Perm       as PP.Perm
   import qualified Data.Algorithm.PP.Utils.List as PP.Utils.List
 
+  -- Array construction auxiliary function.
   mkArray :: [a] -> Array.Array Int a
   mkArray xs = Array.array (0, n-1) $ L.zip [0..] xs
     where
       n = L.length xs
 
+  -- Swap two elements in an aray
   swap :: (Array.Ix i) => i -> i -> Array.Array i x -> Array.Array i x
-  swap i j a = a // [(i, xj), (j, xi)]
-    where
-      xi = a ! i
-      xj = a ! j
+  swap i j a
+    | i == j    = a
+    | otherwise = a // [(i, xj), (j, xi)]
+      where
+        xi = a ! i
+        xj = a ! j
 
+  -- Rank auxiliary function.
   rank' :: Int -> Array.Array Int Int -> Array.Array Int Int -> (Int, Array.Array Int Int, Array.Array Int Int)
   rank' n a b = (s, a', b')
     where
@@ -42,7 +50,7 @@ where
   --
   -- >>> let n = 3 in [(p, rank1 p) | p <- perms n]
   -- [([1,2,3],5),([2,1,3],2),([3,2,1],3),([2,3,1],0),([3,1,2],1),([1,3,2],4)]
-  -- >>> let n = 3 in and [p == unrank1 n (rank1 p) | p <- perms n]
+  -- >>> let n = 3 in and [p == (fromJust . unrank1 n . rank1) p | p <- perms n]
   -- True
   rank1 :: PP.Perm.Perm -> Int
   rank1 p = rank1' (PP.Perm.len p) a b
@@ -50,6 +58,7 @@ where
       a = mkArray . L.map (\ x -> x-1) $ PP.Perm.getList p
       b = mkArray . L.map (\ y -> y-1) . PP.Perm.getList $ PP.Perm.inv p
 
+  -- rank1 auxiliary function
   rank1' :: Int -> Array.Array Int Int -> Array.Array Int Int -> Int
   rank1' 1 _  _  = 0
   rank1' n a b = s + (n * rank1' (n-1) a' b')
@@ -69,6 +78,7 @@ where
     | r < 0 || r >= product [1..n] = Nothing
     | otherwise                    = Just . PP.Perm.mkPerm . unrank1' n r $ mkArray [0..n-1]
 
+  -- unrank1 auxiliary function.
   unrank1' :: Int -> Int -> Array.Array Int Int -> [Int]
   unrank1' n r a
     | n > 0     = unrank1' (n-1) (r `div` n) (swap (n-1) (r `mod` n) a)
@@ -80,7 +90,7 @@ where
   --
   -- >>> let n = 3 in [(p, rank2 p) | p <- perms n]
   -- [([1,2,3],5),([2,1,3],4),([3,2,1],1),([2,3,1],0),([3,1,2],2),([1,3,2],3)]
-  -- >>> let n = 3 in and [p == unrank2 n (rank2 p) | p <- perms n]
+  -- >>> let n = 3 in and [p == (fromJust . unrank2 n . rank2) p | p <- perms n]
   -- True
   rank2 :: PP.Perm.Perm -> Int
   rank2 p = rank2' (PP.Perm.len p) a b
@@ -88,6 +98,7 @@ where
       a = mkArray . L.map (\ x -> x-1) $ PP.Perm.getList p
       b = mkArray . L.map (\ y -> y-1) . PP.Perm.getList $ PP.Perm.inv p
 
+  -- rank2 auxiliary function.
   rank2' :: Int -> Array.Array Int Int -> Array.Array Int Int -> Int
   rank2' 1 _  _  = 0
   rank2' n a b = s * product [1..n-1] + rank2' (n-1) a' b'
@@ -107,6 +118,7 @@ where
     | r < 0 || r >= product [1..n] = Nothing
     | otherwise                    = Just . PP.Perm.mkPerm . unrank2' n r $ mkArray [0..n-1]
 
+  -- unrank2 auxiliary function.
   unrank2' :: Int -> Int -> Array.Array Int Int -> [Int]
   unrank2' n r a
     | n > 0     = unrank2' (n-1) (r - s*product [1..n-1]) (swap (n-1) s a)
@@ -114,12 +126,31 @@ where
       where
         s = r `div` product [1..n-1]
 
+  -- rand auxiliary function : rank.
   randRank :: Array.Array Int Int -> (PP.Perm.Perm -> Int) -> PP.Perm.Perm -> Int
   randRank a f = (!) a . f
 
+  -- rank auxiliary function : unrank.
   randUnrank :: Array.Array Int Int -> (Int -> Int -> Maybe PP.Perm.Perm) -> Int -> Int -> Maybe PP.Perm.Perm
   randUnrank a f n = f n . (!) a
 
+  -- |'rand' 'g' 'n' takes a random generator 'g' and an integer 'n' and return
+  -- a pair @((r, u), g')@ where @(r, g)@ is a pair of functions (random rank, random unrank)
+  -- and 'g'' is a new random generator.
+  --
+  -- >>> ((rank, unrank), _) = rand (mkStdGen 12345) 3
+  -- >>> :type rank
+  -- rank :: Perm -> Int
+  -- >>> :type unrank
+  -- unrank :: Int -> Int -> Maybe Perm
+  -- >>> let n = 3 in [(p, rank p) | p <- perms n]
+  -- [([1,2,3],5),([2,1,3],4),([3,2,1],2),([2,3,1],3),([3,1,2],1),([1,3,2],0)]
+  -- >>> let n = 3 in and [p == (fromJust . unrank n . rank) p | p <- perms n]
+  -- True
+  -- >>> let n = 3 in [(i, unrank 3 i) | i <- [0..product [1..n]-1]]
+  -- [(0,Just [1,3,2]),(1,Just [3,1,2]),(2,Just [3,2,1]),(3,Just [2,3,1]),(4,Just [2,1,3]),(5,Just [1,2,3])]
+  -- >>> let n = 3 in and [i == (rank . fromJust . unrank n) i | i <- [0..product [1..n]-1]]
+  -- True
   rand :: RandomGen g => g -> Int -> ((PP.Perm.Perm -> Int, Int -> Int -> Maybe PP.Perm.Perm), g)
   rand g n = ((randRank a rank2, randUnrank a' unrank2), g')
     where
