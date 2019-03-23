0b195d93d71f7ce43772c8e118135978b9dcbee5
vialette <vialette@gmail.com>
2019-03-23 12:39:36 +0100

    Organization number.

--- src/Data/Algorithm/PP/Perm/Generator/Oscillating.hs
+++ src/Data/Algorithm/PP/Perm/Generator/Oscillating.hs
@@ -19,7 +19,7 @@ import qualified Data.List     as L
 
 import qualified Data.Algorithm.PP.Perm as PP.Perm
 
-{- | 'oscillatingIncreasing' @n@ return the oscillating increasing permutation of length @n@.
+{- | 'oscillatingIncreasing' @n@ returns the oscillating increasing permutation of length @n@.
 
 >>> oscillatingIncreasing 10
 [3,1,5,2,7,4,9,6,10,8]
@@ -27,7 +27,7 @@ import qualified Data.Algorithm.PP.Perm as PP.Perm
 oscillatingIncreasing :: Int-> PP.Perm.Perm
 oscillatingIncreasing n = PP.Perm.mk . L.take n $ F.concat [[2*k+2, 2*k-1] | k <- [1..]]
 
-{- | 'oscillatingIncreasings' return the infinite list of all oscillating increasing permutations.
+{- | 'oscillatingIncreasings' returns the infinite list of all oscillating increasing permutations.
 
 >>> take 8 oscillatingIncreasings
 [[1],[2,1],[2,1,3],[3,1,4,2],[3,1,4,2,5],[3,1,5,2,6,4],[3,1,5,2,6,4,7],[3,1,5,2,7,4,8,6]]
