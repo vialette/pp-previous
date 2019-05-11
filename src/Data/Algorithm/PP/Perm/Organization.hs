aa1acb64be393459a61c3e648f0c38a3fc4a8a6b
vialette <vialette@gmail.com>
2019-05-11 22:57:51 +0200

    Documentation for Data.Algorithm.PP.Perm.Organization module.

--- src/Data/Algorithm/PP/Perm/Organization.hs
+++ src/Data/Algorithm/PP/Perm/Organization.hs
@@ -10,32 +10,53 @@ Stability   : experimental
 
 module Data.Algorithm.PP.Perm.Organization (
     xOrganization
+  , yOrganization
+  , stronglyEquallyOrganized
+
   , xOrganizationNumber
-  , xPalindromic
-
-    yOrganization
   , yOrganizationNumber
-  , yPalindromic
+  , organizationNumber
 
+  , xPalindromic
+  , yPalindromic
   , palindromic
   ) where
 
 import qualified Data.Foldable as F
 import qualified Data.List     as L
 
-import qualified Data.Algorithm.PP.Utils.Foldable           as PP.Utils.Foldable
-import qualified Data.Algorithm.PP.Utils.List               as PP.Utils.List
-import qualified Data.Algorithm.PP.Perm                     as PP.Perm
+import qualified Data.Algorithm.PP.Geometry.Point  as PP.Geometry.Point
+import qualified Data.Algorithm.PP.Utils.Foldable  as PP.Utils.Foldable
+import qualified Data.Algorithm.PP.Utils.List      as PP.Utils.List
+import qualified Data.Algorithm.PP.Perm            as PP.Perm
 
 {- | 'xOrganization' @p@ returns the xOrganization of permutation @p@.
 
->>> mapM_ print . fmap (id &&& xOrganization) $ perms 3
-([1,2,3],[1,1])
-([2,1,3],[1,2])
-([3,2,1],[1,1])
-([2,3,1],[1,2])
-([3,1,2],[2,1])
-([1,3,2],[2,1])
+>>> mapM_ print . fmap (id &&& xOrganization) $ perms 4
+([1,2,3,4],[1,1,1])
+([2,1,3,4],[1,2,1])
+([3,2,1,4],[1,1,3])
+([2,3,1,4],[1,2,3])
+([3,1,2,4],[2,1,2])
+([1,3,2,4],[2,1,2])
+([4,3,2,1],[1,1,1])
+([3,4,2,1],[1,2,1])
+([3,2,4,1],[1,2,3])
+([4,2,3,1],[2,1,2])
+([2,4,3,1],[2,1,2])
+([2,3,4,1],[1,1,3])
+([4,1,2,3],[3,1,1])
+([1,4,2,3],[3,2,1])
+([1,2,4,3],[1,2,1])
+([4,2,1,3],[2,1,2])
+([2,4,1,3],[2,3,2])
+([2,1,4,3],[1,3,1])
+([4,1,3,2],[3,2,1])
+([1,4,3,2],[3,1,1])
+([1,3,4,2],[2,1,2])
+([4,3,1,2],[1,2,1])
+([3,4,1,2],[1,3,1])
+([3,1,4,2],[2,3,2])
 -}
 xOrganization :: PP.Perm.Perm -> [Int]
 xOrganization = L.map (abs . uncurry (-)) . PP.Utils.List.chunk2 . PP.Perm.getList
@@ -72,6 +93,18 @@ The xOrganization number of a permutation is the sum of the integers of the corr
 xOrganizationNumber :: PP.Perm.Perm -> Int
 xOrganizationNumber = F.sum . xOrganization
 
+{- | 'xPalindromic' @p@  returns true iff the x-organization of the permutation @p@ is
+palindromic.
+
+>>> xPalindromic $ mk [4,1,2,5,6,3]
+True
+>>> xOrganization $ mk [4,1,2,5,6,3]
+[3,1,3,1,3]
+>>> xPalindromic $ mk [4,1,6,2,5,3]
+False
+>>> xOrganization $ mk [4,1,6,2,5,3]
+[3,5,4,3,2]
+-}
 xPalindromic :: PP.Perm.Perm -> Bool
 xPalindromic p = xo == L.reverse xo
   where
@@ -124,6 +157,30 @@ The yOrganization number of a permutation is the sum of the integers of the corr
 yOrganizationNumber :: PP.Perm.Perm -> Int
 yOrganizationNumber = F.sum . yOrganization
 
+organizationNumber :: PP.Perm.Perm -> Int
+organizationNumber p = xon - yon
+  where
+    xon = xOrganizationNumber p
+    yon = yOrganizationNumber p
+
+stronglyEquallyOrganized :: PP.Perm.Perm -> Bool
+stronglyEquallyOrganized p = xo == yo
+  where
+    xo = xOrganization p
+    yo = yOrganization p
+
+{- | 'yPalindromic' @p@  returns true iff the y-organization of the permutation @p@ is
+palindromic.
+
+>>> yPalindromic $ mk [4,1,2,5,6,3]
+True
+>>> yOrganization $ mk [4,1,2,5,6,3]
+[1,3,5,3,1]
+>>> yPalindromic $ mk [4,1,6,2,5,3]
+False
+>>> yOrganization $ mk [4,1,6,2,5,3]
+[2,2,5,4,2]
+-}
 yPalindromic :: PP.Perm.Perm -> Bool
 yPalindromic p = yo == L.reverse yo
   where
