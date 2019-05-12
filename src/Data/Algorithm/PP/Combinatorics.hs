32d706655591681e35f905d2f1b467349318c080
vialette <vialette@gmail.com>
2019-05-12 07:06:50 +0200

    Add kComposition function in Data.Algorithm.PP.Combinatorics module.

--- src/Data/Algorithm/PP/Combinatorics.hs
+++ src/Data/Algorithm/PP/Combinatorics.hs
@@ -1,54 +1,85 @@
-module Data.Algorithm.PP.Combinatorics
-(
-  partitions
-, evenPartitions
-)
-where
+{-|
+Module      : Data.Algorithm.PP.Combinatorics
+Description : Convenient combinatorial functions.
+Copyright   : (c) Stéphane Vialette, 2018-2019
+License     : GPL-3
+Maintainer  : vialette@gmail.com
+Stability   : experimental
 
-  {- | 'partitions' @k@ @n@ returns all ordered partitions of @[n]@ into @k@ non-empty parts.
 
-  >>> partitions 0 5
-  []
-  >>> partitions 1 5
-  [[5]]
-  >>> partitions 2 5
-  [[1,4],[2,3],[3,2],[4,1]]
-  >>> partitions 3 5
-  [[1,1,3],[1,2,2],[1,3,1],[2,1,2],[2,2,1],[3,1,1]]
-  >>> partitions 4 5
-  [[1,1,1,2],[1,1,2,1],[1,2,1,1],[2,1,1,1]]
-  >>> partitions 5 5
-  [[1,1,1,1,1]]
-  >>> partitions 6 5
-  []
-  -}
-  partitions :: Int -> Int -> [[Int]]
-  partitions k n
-    | k > n || k <= 0 = []
-    | otherwise       = aux k n
-      where
-        aux 1  n' = [[n']]
-        aux k' n' = [x : xs | x <- [1..n'-k'+1], xs <- aux (k'-1) (n'-x)]
+-}
 
-  {- | 'evenPartitions' @n@ @k@ returns all ordered partitions of @[n]@ into @k@ even parts.
+module Data.Algorithm.PP.Combinatorics (
+    partitions
+  , evenPartitions
 
-  >>> evenPartitions 1 10
-  [[10]]
-  >>> evenPartitions 2 10
-  [[2,8],[4,6],[6,4],[8,2]]
-  >>> evenPartitions 3 10
-  [[2,2,6],[2,4,4],[2,6,2],[4,2,4],[4,4,2],[6,2,2]]
-  >>> evenPartitions 4 10
-  [[2,2,2,4],[2,2,4,2],[2,4,2,2],[4,2,2,2]]
-  >>> evenPartitions 5 10
-  [[2,2,2,2,2]]
-  >>> evenPartitions 6 10
-  []
-  -}
-  evenPartitions :: Int -> Int -> [[Int]]
-  evenPartitions k n
-    | odd n || k <= 0 = []
-    | otherwise       = aux k n
-      where
-        aux 1  n' = [[n']]
-        aux k' n' = [x : xs | x <- [2,4..n'-(2*(k'-1))], xs <- aux (k'-1) (n'-x)]
+  , kCompositions
+  ) where
+
+import Control.Applicative
+
+{- | 'partitions' @k@ @n@ returns all ordered partitions of @[n]@ into @k@ non-empty parts.
+
+>>> partitions 0 5
+[]
+>>> partitions 1 5
+[[5]]
+>>> partitions 2 5
+[[1,4],[2,3],[3,2],[4,1]]
+>>> partitions 3 5
+[[1,1,3],[1,2,2],[1,3,1],[2,1,2],[2,2,1],[3,1,1]]
+>>> partitions 4 5
+[[1,1,1,2],[1,1,2,1],[1,2,1,1],[2,1,1,1]]
+>>> partitions 5 5
+[[1,1,1,1,1]]
+>>> partitions 6 5
+[]
+-}
+partitions :: Int -> Int -> [[Int]]
+partitions k n
+  | k > n || k <= 0 = []
+  | otherwise       = aux k n
+    where
+      aux 1  n' = [[n']]
+      aux k' n' = [x : xs | x <- [1..n'-k'+1], xs <- aux (k'-1) (n'-x)]
+
+{- | 'evenPartitions' @n@ @k@ returns all ordered partitions of @[n]@ into @k@ even parts.
+
+>>> evenPartitions 1 10
+[[10]]
+>>> evenPartitions 2 10
+[[2,8],[4,6],[6,4],[8,2]]
+>>> evenPartitions 3 10
+[[2,2,6],[2,4,4],[2,6,2],[4,2,4],[4,4,2],[6,2,2]]
+>>> evenPartitions 4 10
+[[2,2,2,4],[2,2,4,2],[2,4,2,2],[4,2,2,2]]
+>>> evenPartitions 5 10
+[[2,2,2,2,2]]
+>>> evenPartitions 6 10
+[]
+-}
+evenPartitions :: Int -> Int -> [[Int]]
+evenPartitions k n
+  | odd n || k <= 0 = []
+  | otherwise       = aux k n
+    where
+      aux 1  n' = [[n']]
+      aux k' n' = [x : xs | x <- [2,4..n'-(2*(k'-1))], xs <- aux (k'-1) (n'-x)]
+
+
+{- | 'kCompositions' @k@ @n@ returns all compositions of the integer @n@ into @k@ parts.
+
+>>> let n = 5 in mapM_ print . map (flip kCompositions n) $ [0..n+1]
+[]
+[[5]]
+[[1,4],[2,3],[3,2],[4,1]]
+[[1,1,3],[1,2,2],[1,3,1],[2,1,2],[2,2,1],[3,1,1]]
+[[1,1,1,2],[1,1,2,1],[1,2,1,1],[2,1,1,1]]
+[[1,1,1,1,1]]
+[]
+-}
+kCompositions :: Int -> Int -> [[Int]]
+kCompositions k n
+  | k <= 0    = []
+  | k == 1    = [[n]]
+  | otherwise = concat [(x:) <$> kCompositions (k-1) (n-x) | x <- [1..n-1]]
--- src/Data/Algorithm/PP/Perm/Organization.hs
+++ src/Data/Algorithm/PP/Perm/Organization.hs
@@ -61,6 +61,53 @@ import qualified Data.Algorithm.PP.Perm            as PP.Perm
 xOrganization :: PP.Perm.Perm -> [Int]
 xOrganization = L.map (abs . uncurry (-)) . PP.Utils.List.chunk2 . PP.Perm.getList
 
+{- | 'yOrganization' @p@ returns the yOrganization of permutation @p@.
+
+>>> mapM_ print . fmap (id &&& yOrganization) $ perms 4
+([1,2,3,4],[1,1,1])
+([2,1,3,4],[1,2,1])
+([3,2,1,4],[1,1,3])
+([2,3,1,4],[2,1,2])
+([3,1,2,4],[1,2,3])
+([1,3,2,4],[2,1,2])
+([4,3,2,1],[1,1,1])
+([3,4,2,1],[1,2,1])
+([3,2,4,1],[2,1,2])
+([4,2,3,1],[2,1,2])
+([2,4,3,1],[3,2,1])
+([2,3,4,1],[3,1,1])
+([4,1,2,3],[1,1,3])
+([1,4,2,3],[2,1,2])
+([1,2,4,3],[1,2,1])
+([4,2,1,3],[1,2,3])
+([2,4,1,3],[2,3,2])
+([2,1,4,3],[1,3,1])
+([4,1,3,2],[2,1,2])
+([1,4,3,2],[3,1,1])
+([1,3,4,2],[3,2,1])
+([4,3,1,2],[1,2,1])
+([3,4,1,2],[1,3,1])
+([3,1,4,2],[2,3,2])
+-}
+yOrganization :: PP.Perm.Perm -> [Int]
+yOrganization = L.map f . PP.Utils.List.chunk2 . L.map PP.Geometry.Point.getX . PP.Geometry.Point.sortOnY . PP.Perm.getPoints
+  where
+    f (x1, x2) = abs (x1 - x2)
+
+{- | 'stronglyEquallyOrganized' @p@  returns true iff the permutations @p@ has identical
+x-organization and y-organization.
+
+>>> stronglyEquallyOrganized $ mk [5,6,4,3,1,2]
+True
+>>> let p = mk [5,6,4,3,1,2] in (xOrganization p, yOrganization p)
+([1,2,1,2,1],[1,2,1,2,1])
+-}
+stronglyEquallyOrganized :: PP.Perm.Perm -> Bool
+stronglyEquallyOrganized p = xo == yo
+  where
+    xo = xOrganization p
+    yo = yOrganization p
+
 {- | 'xOrganizationNumber' @p@ returns the xOrganization number of the permutation @p@.
 The xOrganization number of a permutation is the sum of the integers of the corresponding xOrganization.
 
@@ -89,41 +136,11 @@ The xOrganization number of a permutation is the sum of the integers of the corr
 ([4,3,1,2],4)
 ([3,4,1,2],5)
 ([3,1,4,2],7)
+>>> let f = (minimum &&& maximum) . map xOrganizationNumber . perms in [f i | i <- [2..10]]
+[(1,1),(2,3),(3,7),(4,11),(5,17),(6,23),(7,31),(8,39),(9,49)]
 -}
 xOrganizationNumber :: PP.Perm.Perm -> Int
 xOrganizationNumber = F.sum . xOrganization
-
-{- | 'xPalindromic' @p@  returns true iff the x-organization of the permutation @p@ is
-palindromic.
-
->>> xPalindromic $ mk [4,1,2,5,6,3]
-True
->>> xOrganization $ mk [4,1,2,5,6,3]
-[3,1,3,1,3]
->>> xPalindromic $ mk [4,1,6,2,5,3]
-False
->>> xOrganization $ mk [4,1,6,2,5,3]
-[3,5,4,3,2]
--}
-xPalindromic :: PP.Perm.Perm -> Bool
-xPalindromic p = xo == L.reverse xo
-  where
-    xo = xOrganization p
-
-{- | 'yOrganization' @p@ returns the yOrganization of permutation @p@.
-
->>> mapM_ print . fmap (id &&& yOrganization) $ perms 3
-([1,2,3],[1,1])
-([2,1,3],[1,2])
-([3,2,1],[1,1])
-([2,3,1],[2,1])
-([3,1,2],[1,2])
-([1,3,2],[2,1])
--}
-yOrganization :: PP.Perm.Perm -> [Int]
-yOrganization = L.map f . PP.Utils.List.chunk2 . L.map PP.Geometry.Point.getX . PP.Geometry.Point.sortOnY . PP.Perm.getPoints
-  where
-    f (x1, x2) = abs (x1 - x2)
 
 {- | 'yOrganizationNumber' @p@ returns the yOrganization number of the permutation @p@.
 The yOrganization number of a permutation is the sum of the integers of the corresponding yOrganization.
@@ -153,38 +170,81 @@ The yOrganization number of a permutation is the sum of the integers of the corr
 ([4,3,1,2],4)
 ([3,4,1,2],5)
 ([3,1,4,2],7)
+>>> let f = (minimum &&& maximum) . map yOrganizationNumber . perms in [f i | i <- [2..10]]
+[(1,1),(2,3),(3,7),(4,11),(5,17),(6,23),(7,31),(8,39),(9,49)]
 -}
 yOrganizationNumber :: PP.Perm.Perm -> Int
 yOrganizationNumber = F.sum . yOrganization
 
+{- | 'organizationNumber' @p@ returns the difference between the x-organization number
+and the y-organization number of the permutation @p@.
+
+>>> let p = mk [5,1,6,4,3,2] in organizationNumber p
+2
+>>> let p = mk [5,1,6,4,3,2]  in (xOrganizationNumber p, yOrganizationNumber p)
+(13,11)
+>>> let p = mk [2,1,5,6,4,3] in organizationNumber p
+-1
+>>> let p = mk [2,1,5,6,4,3] in (xOrganizationNumber p, yOrganizationNumber p)
+(9,10)
+>>> let p = mk [5,6,4,3,1,2] in organizationNumber p
+0
+>>> let p = mk [5,6,4,3,1,2] in (xOrganizationNumber p, yOrganizationNumber p)
+(7,7)
+-}
 organizationNumber :: PP.Perm.Perm -> Int
 organizationNumber p = xon - yon
   where
     xon = xOrganizationNumber p
     yon = yOrganizationNumber p
 
-stronglyEquallyOrganized :: PP.Perm.Perm -> Bool
-stronglyEquallyOrganized p = xo == yo
+{- | 'xPalindromic' @p@  returns true iff the x-organization of the permutation @p@ is
+palindromic.
+
+>>> xPalindromic $ mk [4,1,2,5,6,3]
+True
+>>> xOrganization $ mk [4,1,2,5,6,3]
+[3,1,3,1,3]
+>>> xPalindromic $ mk [4,1,6,2,5,3]
+False
+>>> xOrganization $ mk [4,1,6,2,5,3]
+[3,5,4,3,2]
+>>> let f = length . filter yPalindromic . perms in [f i | i <- [2..10]]
+[2,2,16,8,88,60,960,424,6656]
+-}
+xPalindromic :: PP.Perm.Perm -> Bool
+xPalindromic p = xo == L.reverse xo
   where
     xo = xOrganization p
-    yo = yOrganization p
 
 {- | 'yPalindromic' @p@  returns true iff the y-organization of the permutation @p@ is
 palindromic.
 
->>> yPalindromic $ mk [4,1,2,5,6,3]
+>>> let p = mk [4,1,2,5,6,3] in yPalindromic p
 True
->>> yOrganization $ mk [4,1,2,5,6,3]
+>>> let p = mk [4,1,2,5,6,3] in yOrganization p
 [1,3,5,3,1]
->>> yPalindromic $ mk [4,1,6,2,5,3]
+>>> let p = mk [4,1,6,2,5,3] in yPalindromic p
 False
->>> yOrganization $ mk [4,1,6,2,5,3]
+>>> let p = mk [4,1,6,2,5,3] in yOrganization p
 [2,2,5,4,2]
+>>> let f = length . filter yPalindromic . perms in [f i | i <- [2..10]]
+[2,2,16,8,88,60,960,424,6656]
 -}
 yPalindromic :: PP.Perm.Perm -> Bool
 yPalindromic p = yo == L.reverse yo
   where
     yo = yOrganization p
 
+{- | 'palindromic' @p@ returns true iff both the x-organization and the y-organization
+of the permutation @p@ are palindromic.
+
+>>> let p = mk [4,5,6,1,2,3] in palindromic p
+True
+>>> let p = mk [4,5,6,1,2,3] in (xOrganization p, yOrganization p)
+([1,1,5,1,1],[1,1,5,1,1])
+>>> let f = length . filter palindromic . perms in [f i | i <- [2..10]]
+[2,2,12,8,56,48,444,384,3856]
+-}
 palindromic :: PP.Perm.Perm -> Bool
 palindromic p = xPalindromic p && yPalindromic p
