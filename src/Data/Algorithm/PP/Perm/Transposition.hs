3fa74c59270c7e6b1ab575d3eb6d41c99fc41f48
vialette <vialette@gmail.com>
2019-05-16 16:09:12 +0200

    Data.Algorithm.PP.Perm.Transposition.

--- src/Data/Algorithm/PP/Perm/Transposition.hs
+++ src/Data/Algorithm/PP/Perm/Transposition.hs
@@ -11,6 +11,7 @@ Stability   : experimental
 module Data.Algorithm.PP.Perm.Transposition (
     transpose
   , transpositions
+  , adjacentTranspositions
   ) where
 
 import qualified Control.Arrow as A
@@ -22,7 +23,7 @@ import qualified Data.Algorithm.PP.Perm                 as PP.Perm
 import qualified Data.Algorithm.PP.Utils.List           as PP.Utils.List
 
 
-{- | 'transpose' @i@ @j@ @p@ returns the transposition @(i,j)@ of permutation @p@.
+{-| 'transpose' @i@ @j@ @p@ returns the transposition @(i,j)@ of permutation @p@.
 
 >>>
 
@@ -30,10 +31,15 @@ import qualified Data.Algorithm.PP.Utils.List           as PP.Utils.List
 transpose :: Int -> Int -> PP.Perm.Perm -> PP.Perm.Perm
 transpose i j = PP.Perm.mk . PP.Utils.List.swapElementsAt i j . PP.Perm.getList
 
-{- | 'transpositions' @p@ returns all transpositions of permutation @p@.
+{-| 'transpositions' @p@ returns all transpositions of permutation @p@.
 
 -}
 transpositions :: PP.Perm.Perm -> [PP.Perm.Perm]
 transpositions p = [transpose i j p | i <- [0..n-2], j <- [i+1..n-1]]
+  where
+    n = PP.Perm.len p
+
+adjacentTranspositions :: PP.Perm.Perm -> [PP.Perm.Perm]
+adjacentTranspositions p = [transpose i (i+1) p | i <- [0..n-2]]
   where
     n = PP.Perm.len p
