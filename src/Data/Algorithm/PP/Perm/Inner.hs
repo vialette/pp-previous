f3c5c18832f3a85f83e4bf1ed12055bae23cebbc
vialette <vialette@gmail.com>
2019-06-11 06:36:13 +0200

    Add src/Data/Algorithm/PP/Perm/Inner.hs file.

---
+++ src/Data/Algorithm/PP/Perm/Inner.hs
@@ -0,0 +1,12 @@
+{-|
+Module      : Data.Algorithm.PP.Perm.Pattern.Inner
+Description : Permutation inner type
+Copyright   : (c) Stéphane Vialette, 2018-2019
+License     : GPL-3
+Maintainer  : vialette@gmail.com
+Stability   : experimental
+
+Permutation internal.
+-}
+
+module Data.Algorithm.PP.Perm.Pattern.Inner () where
